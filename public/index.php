977db73bf4124849939dca7b8365d24d27cdb516
Maxime JOASSY <maximejoassy@hotmail.fr>
2018-04-14 18:17:43 +0200

    :package: Add BlogModule

--- public/index.php
+++ public/index.php
@@ -2,7 +2,9 @@
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-$app = new \Framework\App();
+$app = new \Framework\App([
+    \App\Blog\BlogModule::class
+]);
 
 $response = $app->run(\GuzzleHttp\Psr7\ServerRequest::fromGlobals());
 
--- src/Framework/App.php
+++ src/Framework/App.php
@@ -14,14 +14,19 @@ class App
     private $modules;
 
     /**
+     * @var Router
+     */
+    private $router;
+
+    /**
      * App constructor.
      * @param string[] $modules Modules list to load
      */
     public function __construct(array $modules = [])
     {
-        $router = new Router();
+        $this->router = new Router();
         foreach ($modules as $module) {
-            $this->modules[] = new $module();
+            $this->modules[] = new $module($this->router);
         }
     }
 
@@ -36,10 +41,23 @@ class App
             );
         }
 
-        if ($uri === '/blog') {
-            return new Response('200', [], '<h1>Bienvenue sur le blog</h1>');
+        $route = $this->router->match($request);
+
+        if (is_null($route)) {
+            return new Response('404', [], 'Error 404');
         }
 
-        return new Response('404', [], 'Error 404');
+        $params = $route->getParams();
+        $request = array_reduce(array_keys($params), function ($request, $key) use ($params) {
+            return $request->withAttribute($key, $params[$key]);
+        }, $request);
+
+        $response = call_user_func_array($route->getCallBack(), [$request]);
+
+        if (is_string($response)) {
+            return new Response(200, [], $response);
+        } elseif ($response instanceof ResponseInterface) {
+            return $response;
+        }
     }
 }
